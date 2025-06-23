ee03bd881379bc00d01480e10e507df420de7a72
Muhammad Arief Rahman <arief21si@mahasiswa.pcr.ac.id>
2025-06-23 15:44:49 +0700

    Add initial project setup with dependencies and basic server configuration
    - Updated bun.lock with new dependencies including shared-utils, express, multer, and zod.
    - Created index.ts with Express server setup and middleware for JSON parsing and file uploads.
    - Added .env.example for environment variable configuration.
    - Initialized empty route and maintenance controller files.

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { response } from '@ariefrahman39/shared-utils'
+import express from 'express';
+import multer from 'multer';
+
+const app = express()
+const port = process.env.PORT || 5100;
+
+app.use(express.json({ limit: '50mb' }))
+app.use(multer().any())
+
+app.use((req, res) => {
+  response(res, 404, 'Not Found')
+})
+
+app.listen(port, () => {
+  console.log(`Aggregator-Service is running on port ${port}`)
+})
